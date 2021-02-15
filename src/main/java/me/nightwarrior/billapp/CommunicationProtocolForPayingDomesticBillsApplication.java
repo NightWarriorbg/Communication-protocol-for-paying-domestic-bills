9471f417e78daa600e421198f53c01df3d35049b
Radoslav Enev <nightWarrior.bg@gmail.com>
2021-02-15 15:21:06 +0200

    Initial commit

---
+++ src/main/java/me/nightwarrior/billapp/CommunicationProtocolForPayingDomesticBillsApplication.java
@@ -0,0 +1,13 @@
+package me.nightwarrior.billapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationProtocolForPayingDomesticBillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationProtocolForPayingDomesticBillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nightwarrior/billapp/CommunicationProtocolForPayingDomesticBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nightwarrior.billapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationProtocolForPayingDomesticBillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
